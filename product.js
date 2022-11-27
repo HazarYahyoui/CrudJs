60dc17b5740977e1d059f4d30e51f952a2832863
HazarYahyaoui <yahyaoui.hazar@gmail.com>
2022-11-27 13:04:33 +0100

    Fonction Add

--- product.js
+++ product.js
@@ -3,14 +3,14 @@ var Name = document.getElementById('nom');
 var Description = document.getElementById('description');
 var Price = document.getElementById('prix');
 var Quantity = document.getElementById('quantité');
-addition.addEventListener('click',F_add);
+// addition.addEventListener('click',F_add);
 var v=true
 function F_add(event){
     event.preventDefault();
     if (Name.value=='') {
         Name.classList.add('is-invalid');
         Name.classList.remove('is-valid');
-        var v = false ;
+         v = false ;
     }else {
         Name.classList.add('is-valid');
         Name.classList.remove('is-invalid');
@@ -18,7 +18,7 @@ function F_add(event){
     if (Description.value=='') {
         Description.classList.add('is-invalid');
         Description.classList.remove('is-valid');
-        var v = false ;
+        v = false ;
     }else {
         Description.classList.add('is-valid');
         Description.classList.remove('is-invalid');
@@ -26,7 +26,7 @@ function F_add(event){
     if (Price.value=='') {
         Price.classList.add('is-invalid');
         Price.classList.remove('is-valid');
-        var v = false ;
+         v = false ;
     }else {
         Price.classList.add('is-valid');
         Price.classList.remove('is-invalid');
@@ -34,20 +34,21 @@ function F_add(event){
     if (Quantity.value=='') {
         Quantity.classList.add('is-invalid');
         Quantity.classList.remove('is-valid');
-        var v = false ;
+         v = false ;
     }else {
         Quantity.classList.add('is-valid');
         Quantity.classList.remove('is-invalid');
     }
-    if (v==true) {
-    var produit = JSON.parse(localStorage.getItem('produit') || '[]') ;
+    if (v == true) {
+    var produit = JSON.parse(localStorage.getItem('produit')) || [] ;
     var prod ={
         Name : Name.value,
         Description : Description.value,
         Price : Price.value,
         Quantity : Quantity.value
     }
+    produit.push(prod);
     localStorage.setItem('produit',JSON.stringify(produit));
-    document.location.href='listProduct.html'
+    document.location.href = 'listProduct.html';
     }
 }
